6248235a6d859efb9c8205742d268400330d0b18
Paul Bruynel <paulbruynel@MacBook-Air-van-Paul.local>
2020-11-15 19:49:36 +0100

    3nd plot  - R script to generate plot3.png  - plot3.png

--- plot3.R
+++ plot3.R
@@ -11,7 +11,10 @@ col1 <- paste(df1$Date, df1$Time)
 df1 <- cbind(strptime(col1, format = "%d/%m/%Y %H:%M:%S"), df1[,c(3:9)])
 names(df1)[1] <- "Datetime"
 
-# Plot the Global Active Power over the two days in a PNG file named plot2.png
-png(file = "plot2.png")
-plot(df1$Datetime, df1$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
+# Plot the Energy sub metering 1, 2 and 3 over the two days in a PNG file named plot3.png
+png(file = "plot3.png")
+plot(df1$Datetime, df1$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l", col = "black")
+points(df1$Datetime, df1$Sub_metering_2, type = "l", col = "red")
+points(df1$Datetime, df1$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), , lty=1, col=c("black", "red", "blue"))
 dev.off()
