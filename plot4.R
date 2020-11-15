69452b1baaaea319e97fbe1bedae6d2e40a73bd1
Paul Bruynel <paulbruynel@gmail.com>
2020-11-15 20:46:15 +0100

    4th plot  - R script for plot4.png  - plot4.png

---
+++ plot4.R
@@ -0,0 +1,28 @@
+# Read the data.
+# Read from file household_power_consumption.txt.
+# Only lines that start with 1/2/2007 or 2/2/2007.
+library(data.table)
+colNames <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+colCls = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+df1 <- fread(cmd = "grep -E '^1/2/2007|^2/2/2007' household_power_consumption.txt", sep = ";", na.strings="?", col.names = colNames, colClasses = colCls)
+
+# Merge columns Date and Time into one column Datetime
+col1 <- paste(df1$Date, df1$Time)
+df1 <- cbind(strptime(col1, format = "%d/%m/%Y %H:%M:%S"), df1[,c(3:9)])
+names(df1)[1] <- "Datetime"
+
+# Plot 4 graphs in a PNG file named plot4.png
+png(file = "plot4.png")
+par(mfcol = c(2,2))
+# Plot the Global Active Power over the two days
+plot(df1$Datetime, df1$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
+# Plot the Energy sub metering 1, 2 and 3 over the two days
+plot(df1$Datetime, df1$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l", col = "black")
+points(df1$Datetime, df1$Sub_metering_2, type = "l", col = "red")
+points(df1$Datetime, df1$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"), box.lty=0)
+# Plot the Voltage over the two days
+plot(df1$Datetime, df1$Voltage, xlab = "datetime", ylab = "Voltage", type = "l")
+# Plot the Global Reactive Power over the two days
+plot(df1$Datetime, df1$Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power", type = "l")
+dev.off()
